5d88085eadc06df856faee88d43250d9f3eb351f
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-06-26 13:27:27 +0900

    chore : first commit

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1719375978',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
